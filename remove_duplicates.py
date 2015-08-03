c42b99ddef2c7eb24fbaf1c7c050fa775733b6a7
Tori <victorianelson2016@u.northwestern.edu>
2015-08-03 14:38:15 -0700

    help

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -2,7 +2,7 @@
 #Created by Victoria Nelson July 31,2015
 #Takes vertex and tri lists
 #Removes duplicated vertexes and updates tri list
-#IMPORTANT NOTE: ID starts at 1
+#IMPORTANT NOTE: ID in tri list starts at 1
 __author__ = 'Tori'
 import os
 import sys
@@ -12,6 +12,7 @@ vlines = vfile.readlines()
 verts = vlines
 
 nv = len(vlines)
+print nv
 trifile = open("mb_tri_all_27_6.dat",'r')
 tlines = trifile.readlines()
 ntri = len(tlines)
@@ -23,21 +24,24 @@ for j in range(ntri): #Create matrix of the triangle list - tested
         tri[j][k] = int(tl[k])
 s = 0
 for v in vlines:
-    if s > 5: break
+    if s > 10: break
 
     coo = v.split()
     vx = float(coo[0])
     vy = float(coo[1])
     vz = float(coo[2])
+    m = vlines.index(v)
     for vert in verts:
         ver = vert.split()
         vertx = float(ver[0])
         verty = float(ver[1])
         vertz = float(ver[2])
-        if (vertx == vx and verty == vy and vertz == vz and verts.index(vert) > vlines.index(v)):#Finds duplicate vertex
-            print vx,vertx
+
+        if vertx == vx and verty == vy and vertz == vz and verts.index(vert) > m:#Finds duplicate vertex
+            num = verts.count(v)
+            print vert, len(verts), num
             n = verts.index(vert)
-            m = vlines.index(v)
+            #m = vlines.index(v)
             print m,n
             del verts[n] #Delete duplicate vertex
             s = s+1
@@ -48,6 +52,7 @@ for v in vlines:
                         tri[i][j] = tri[i][j] - 1
                     elif tri[i][j] == (n+1):#Replace w/ new ID
                         tri[i][j] = m+1
+    vlines = verts
 
 vertout = open("mb_clean_verts_all_27_6.dat",'w')
 for vert in verts:
